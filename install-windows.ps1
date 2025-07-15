e2af804aca2c4efccc0994877e950457d6876140
Horizon <mgthedasher@gmail.com>
2025-07-15 19:56:09 +0300

    init

---
+++ install-windows.ps1
@@ -0,0 +1,6 @@
+Move-Item $env:LOCALAPPDATA\nvim $env:LOCALAPPDATA\nvim.bak
+Move-Item $env:LOCALAPPDATA\nvim-data $env:LOCALAPPDATA\nvim-data.bak
+
+git clone https://github.com/LazyVim/starter $env:LOCALAPPDATA\nvim
+
+Remove-Item $env:LOCALAPPDATA\nvim\.git -Recurse -Force
