b38c4c765983320cc312fa28325cc233d5e4c1e5
Horizon <mgthedasher@gmail.com>
2025-07-15 19:53:41 +0300

    init

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("config.lazy")
+-- This file is automatically loaded by init.lua
+-- You can add your own custom plugins here, or override existing ones.
+-- For more information, see: lazyvim.org
+-- You can also use this file to set up your own custom configurations.
---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "Welcome to the LazyVim Easy Installation!"
+
+mv ~/.config/nvim{,.bak}
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
+
+echo "Done!"
